9c9ad0c5183fc06a5e0d5e84c09d37bcd4df2dd1
Richard Huang <flyerhzm@gmail.com>
2012-05-12 08:59:17 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsBrakemanCom::Application.config.secret_token = 'c650cf53289a0f5bbaa0c9eea27d18ec51b7a4c9207ddef997521bd7c6d683f5167cca02079cbf105b41e46c0f53f7e094100e9eb292ccf7c8f34e36dbb4c3e4'
