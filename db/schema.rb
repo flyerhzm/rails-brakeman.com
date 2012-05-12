f51d1312c653854664f4755e3d83d0c65f6fdfe1
Richard Huang <flyerhzm@gmail.com>
2012-05-12 17:06:04 +0800

    add delayed_jobs table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120512084245) do
+ActiveRecord::Schema.define(:version => 20120512090522) do
 
   create_table "builds", :force => true do |t|
     t.integer  "repository_id"
@@ -27,6 +27,22 @@ ActiveRecord::Schema.define(:version => 20120512084245) do
   end
 
   add_index "builds", ["repository_id"], :name => "index_builds_on_repository_id"
+
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
 
   create_table "repositories", :force => true do |t|
     t.integer  "github_id"
