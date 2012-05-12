c513b5fa5cdf3c23f90d5823cef6470c9f4169b4
Richard Huang <flyerhzm@gmail.com>
2012-05-12 16:28:09 +0800

    add relationship from repository to builds

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,4 +1,5 @@
 class Repository < ActiveRecord::Base
+  has_many :builds
   attr_accessible :description, :fork, :github_id, :github_name, :git_url, :name, :private, :pushed_at
   before_create :sync_github
 
