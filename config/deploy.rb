c39f0a8e3012be1ba2b96557967801d340dabda9
Richard Huang <flyerhzm@gmail.com>
2012-06-16 21:17:01 +0800

    update rvm env in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ require "delayed/recipes"
 
 $:.unshift(File.expand_path('./lib', ENV['rvm_path']))
 require 'rvm/capistrano'
-set :rvm_ruby_string, 'ruby-1.9.3-p125@rails-brakeman.com'
+set :rvm_ruby_string, 'ruby-1.9.3-p194@rails-brakeman.com'
 set :rvm_type, :user
 
 set :application, "rails-brakeman.com"
