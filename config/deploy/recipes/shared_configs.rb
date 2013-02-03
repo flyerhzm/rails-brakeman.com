df8546be2b940d28225b2c09cdacb5261fa2513b
Richard Huang <flyerhzm@gmail.com>
2013-02-03 16:52:43 +0800

    remove secret_token

--- config/deploy/recipes/shared_configs.rb
+++ config/deploy/recipes/shared_configs.rb
@@ -4,6 +4,7 @@ namespace :deploy do
   task :update_shared_symlink do
     run "ln -nfs #{shared_path}/config/*.yml #{release_path}/config/"
     run "ln -nfs #{shared_path}/builds #{release_path}/builds"
+    run "ln -nfs #{shared_path}/config/initializers/secret_token.rb #{release_path}/config/initializers/secret_token.rb"
   end
 end
 
