ce2d4098393de6038e095501d0f42719f5469b60
Richard Huang <flyerhzm@gmail.com>
2013-02-03 20:01:46 +0800

    rails_admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,162 +1,222 @@
-# RailsAdmin config file. Generated on August 05, 2012 11:34
+# RailsAdmin config file. Generated on February 03, 2013 20:01
 # See github.com/sferik/rails_admin for more informations
 
 RailsAdmin.config do |config|
 
-  # If your default_local is different from :en, uncomment the following 2 lines and set your default locale here:
-  # require 'i18n'
-  # I18n.default_locale = :de
 
+  ################  Global configuration  ################
+
+  # Set the admin name here (optional second array element will appear in red). For example:
+  config.main_app_name = ['Rails Brakeman Com', 'Admin']
+  # or for a more dynamic name:
+  # config.main_app_name = Proc.new { |controller| [Rails.application.engine_name.titleize, controller.params['action'].titleize] }
+
+  # RailsAdmin may need a way to know who the current user is]
   config.current_user_method { current_user } # auto-generated
 
   # If you want to track changes on your models:
-  # config.audit_with :history, User
+  # config.audit_with :history, 'User'
 
   # Or with a PaperTrail: (you need to install it first)
-  # config.audit_with :paper_trail, User
+  # config.audit_with :paper_trail, 'User'
 
-  # Set the admin name here (optional second array element will appear in a beautiful RailsAdmin red ©)
-  config.main_app_name = ['Rails Brakeman Com', 'Admin']
-  # or for a dynamic name:
-  # config.main_app_name = Proc.new { |controller| [Rails.application.engine_name.titleize, controller.params['action'].titleize] }
-
-  config.authorize_with :cancan
-
-  #  ==> Global show view settings
-  # Display empty fields in show views
+  # Display empty fields in show views:
   # config.compact_show_view = false
 
-  #  ==> Global list view settings
   # Number of default rows per-page:
   # config.default_items_per_page = 20
 
-  #  ==> Included models
-  # Add all excluded models here:
-  # config.excluded_models = [Build, Repository, User]
+  # Exclude specific models (keep the others):
+  # config.excluded_models = ['Build', 'Repository', 'User']
 
-  # Add models here if you want to go 'whitelist mode':
-  # config.included_models = [Build, Repository, User]
+  # Include specific models (exclude the others):
+  # config.included_models = ['Build', 'Repository', 'User']
 
-  # Application wide tried label methods for models' instances
+  # Label methods for model instances:
   # config.label_methods << :description # Default is [:name, :title]
 
-  #  ==> Global models configuration
-  # config.models do
-  #   # Configuration here will affect all included models in all scopes, handle with care!
-  #
-  #   list do
-  #     # Configuration here will affect all included models in list sections (same for show, export, edit, update, create)
-  #
-  #     fields_of_type :date do
-  #       # Configuration here will affect all date fields, in the list section, for all included models. See README for a comprehensive type list.
-  #     end
-  #   end
-  # end
-  #
-  #  ==> Model specific configuration
-  # Keep in mind that *all* configuration blocks are optional.
-  # RailsAdmin will try his best to provide the best defaults for each section, for each field.
-  # Try to override as few things as possible, in the most generic way. Try to avoid setting labels for models and attributes, use ActiveRecord I18n API instead.
-  # Less code is better code!
-  # config.model MyModel do
-  #   # Cross-section field configuration
-  #   object_label_method :name     # Name of the method called for pretty printing an *instance* of ModelName
-  #   label 'My model'              # Name of ModelName (smartly defaults to ActiveRecord's I18n API)
-  #   label_plural 'My models'      # Same, plural
-  #   weight -1                     # Navigation priority. Bigger is higher.
-  #   parent OtherModel             # Set parent model for navigation. MyModel will be nested below. OtherModel will be on first position of the dropdown
-  #   navigation_label              # Sets dropdown entry's name in navigation. Only for parents!
+
+  ################  Model configuration  ################
+
+  # Each model configuration can alternatively:
+  #   - stay here in a `config.model 'ModelName' do ... end` block
+  #   - go in the model definition file in a `rails_admin do ... end` block
+
+  # This is your choice to make:
+  #   - This initializer is loaded once at startup (modifications will show up when restarting the application) but all RailsAdmin configuration would stay in one place.
+  #   - Models are reloaded at each request in development mode (when modified), which may smooth your RailsAdmin development workflow.
+
+
+  # Now you probably need to tour the wiki a bit: https://github.com/sferik/rails_admin/wiki
+  # Anyway, here is how RailsAdmin saw your application's models when you ran the initializer:
+
+
+
+  ###  Build  ###
+
+  # config.model 'Build' do
+
+  #   # You can copy this to a 'rails_admin do ... end' block inside your build.rb model definition
+
+  #   # Found associations:
+
+  #     configure :repository, :belongs_to_association 
+
+  #   # Found columns:
+
+  #     configure :id, :integer 
+  #     configure :repository_id, :integer         # Hidden 
+  #     configure :last_commit_id, :string 
+  #     configure :last_commit_message, :string 
+  #     configure :position, :integer 
+  #     configure :duration, :integer 
+  #     configure :finished_at, :datetime 
+  #     configure :branch, :string 
+  #     configure :created_at, :datetime 
+  #     configure :updated_at, :datetime 
+  #     configure :aasm_state, :string 
+  #     configure :warnings_count, :integer 
+
+  #   # Cross-section configuration:
+
+  #     # object_label_method :name     # Name of the method called for pretty printing an *instance* of ModelName
+  #     # label 'My model'              # Name of ModelName (smartly defaults to ActiveRecord's I18n API)
+  #     # label_plural 'My models'      # Same, plural
+  #     # weight 0                      # Navigation priority. Bigger is higher.
+  #     # parent OtherModel             # Set parent model for navigation. MyModel will be nested below. OtherModel will be on first position of the dropdown
+  #     # navigation_label              # Sets dropdown entry's name in navigation. Only for parents!
+
   #   # Section specific configuration:
-  #   list do
-  #     filters [:id, :name]  # Array of field names which filters should be shown by default in the table header
-  #     items_per_page 100    # Override default_items_per_page
-  #     sort_by :id           # Sort column (default is primary key)
-  #     sort_reverse true     # Sort direction (default is true for primary key, last created first)
-  #     # Here goes the fields configuration for the list view
-  #   end
+
+  #     list do
+  #       # filters [:id, :name]  # Array of field names which filters should be shown by default in the table header
+  #       # items_per_page 100    # Override default_items_per_page
+  #       # sort_by :id           # Sort column (default is primary key)
+  #       # sort_reverse true     # Sort direction (default is true for primary key, last created first)
+  #     end
+  #     show do; end
+  #     edit do; end
+  #     export do; end
+  #     # also see the create, update, modal and nested sections, which override edit in specific cases (resp. when creating, updating, modifying from another model in a popup modal or modifying from another model nested form)
+  #     # you can override a cross-section field configuration in any section with the same syntax `configure :field_name do ... end`
+  #     # using `field` instead of `configure` will exclude all other fields and force the ordering
   # end
 
-  # Your model's configuration, to help you get started:
 
-  # All fields marked as 'hidden' won't be shown anywhere in the rails_admin unless you mark them as visible. (visible(true))
+  ###  Repository  ###
 
-  # config.model Build do
+  # config.model 'Repository' do
+
+  #   # You can copy this to a 'rails_admin do ... end' block inside your repository.rb model definition
+
   #   # Found associations:
-  #     configure :repository, :belongs_to_association   #   # Found columns:
-  #     configure :id, :integer
-  #     configure :repository_id, :integer         # Hidden
-  #     configure :last_commit_id, :string
-  #     configure :last_commit_message, :string
-  #     configure :position, :integer
-  #     configure :duration, :integer
-  #     configure :finished_at, :datetime
-  #     configure :branch, :string
-  #     configure :created_at, :datetime
-  #     configure :updated_at, :datetime
-  #     configure :aasm_state, :string
-  #     configure :warnings_count, :integer   #   # Sections:
-  #   list do; end
-  #   export do; end
-  #   show do; end
-  #   edit do; end
-  #   create do; end
-  #   update do; end
+
+  #     configure :user, :belongs_to_association 
+  #     configure :builds, :has_many_association 
+
+  #   # Found columns:
+
+  #     configure :id, :integer 
+  #     configure :github_id, :integer 
+  #     configure :github_name, :string 
+  #     configure :name, :string 
+  #     configure :description, :string 
+  #     configure :git_url, :string 
+  #     configure :private, :boolean 
+  #     configure :fork, :boolean 
+  #     configure :pushed_at, :datetime 
+  #     configure :created_at, :datetime 
+  #     configure :updated_at, :datetime 
+  #     configure :last_build_at, :datetime 
+  #     configure :html_url, :string 
+  #     configure :ssh_url, :string 
+  #     configure :builds_count, :integer 
+  #     configure :user_id, :integer         # Hidden 
+  #     configure :authentication_token, :string 
+  #     configure :visible, :boolean 
+  #     configure :rails, :boolean 
+
+  #   # Cross-section configuration:
+
+  #     # object_label_method :name     # Name of the method called for pretty printing an *instance* of ModelName
+  #     # label 'My model'              # Name of ModelName (smartly defaults to ActiveRecord's I18n API)
+  #     # label_plural 'My models'      # Same, plural
+  #     # weight 0                      # Navigation priority. Bigger is higher.
+  #     # parent OtherModel             # Set parent model for navigation. MyModel will be nested below. OtherModel will be on first position of the dropdown
+  #     # navigation_label              # Sets dropdown entry's name in navigation. Only for parents!
+
+  #   # Section specific configuration:
+
+  #     list do
+  #       # filters [:id, :name]  # Array of field names which filters should be shown by default in the table header
+  #       # items_per_page 100    # Override default_items_per_page
+  #       # sort_by :id           # Sort column (default is primary key)
+  #       # sort_reverse true     # Sort direction (default is true for primary key, last created first)
+  #     end
+  #     show do; end
+  #     edit do; end
+  #     export do; end
+  #     # also see the create, update, modal and nested sections, which override edit in specific cases (resp. when creating, updating, modifying from another model in a popup modal or modifying from another model nested form)
+  #     # you can override a cross-section field configuration in any section with the same syntax `configure :field_name do ... end`
+  #     # using `field` instead of `configure` will exclude all other fields and force the ordering
   # end
-  # config.model Repository do
+
+
+  ###  User  ###
+
+  # config.model 'User' do
+
+  #   # You can copy this to a 'rails_admin do ... end' block inside your user.rb model definition
+
   #   # Found associations:
-  #     configure :user, :belongs_to_association
-  #     configure :builds, :has_many_association   #   # Found columns:
-  #     configure :id, :integer
-  #     configure :github_id, :integer
-  #     configure :github_name, :string
-  #     configure :name, :string
-  #     configure :description, :string
-  #     configure :git_url, :string
-  #     configure :private, :boolean
-  #     configure :fork, :boolean
-  #     configure :pushed_at, :datetime
-  #     configure :created_at, :datetime
-  #     configure :updated_at, :datetime
-  #     configure :last_build_at, :datetime
-  #     configure :html_url, :string
-  #     configure :ssh_url, :string
-  #     configure :builds_count, :integer
-  #     configure :user_id, :integer         # Hidden
-  #     configure :authentication_token, :string
-  #     configure :visible, :boolean   #   # Sections:
-  #   list do; end
-  #   export do; end
-  #   show do; end
-  #   edit do; end
-  #   create do; end
-  #   update do; end
+
+  #     configure :repositories, :has_many_association 
+
+  #   # Found columns:
+
+  #     configure :id, :integer 
+  #     configure :email, :string 
+  #     configure :password, :password         # Hidden 
+  #     configure :password_confirmation, :password         # Hidden 
+  #     configure :reset_password_token, :string         # Hidden 
+  #     configure :reset_password_sent_at, :datetime 
+  #     configure :remember_created_at, :datetime 
+  #     configure :sign_in_count, :integer 
+  #     configure :current_sign_in_at, :datetime 
+  #     configure :last_sign_in_at, :datetime 
+  #     configure :current_sign_in_ip, :string 
+  #     configure :last_sign_in_ip, :string 
+  #     configure :created_at, :datetime 
+  #     configure :updated_at, :datetime 
+  #     configure :github_uid, :integer 
+  #     configure :nickname, :string 
+  #     configure :name, :string 
+  #     configure :github_token, :string 
+  #     configure :admin, :boolean 
+
+  #   # Cross-section configuration:
+
+  #     # object_label_method :name     # Name of the method called for pretty printing an *instance* of ModelName
+  #     # label 'My model'              # Name of ModelName (smartly defaults to ActiveRecord's I18n API)
+  #     # label_plural 'My models'      # Same, plural
+  #     # weight 0                      # Navigation priority. Bigger is higher.
+  #     # parent OtherModel             # Set parent model for navigation. MyModel will be nested below. OtherModel will be on first position of the dropdown
+  #     # navigation_label              # Sets dropdown entry's name in navigation. Only for parents!
+
+  #   # Section specific configuration:
+
+  #     list do
+  #       # filters [:id, :name]  # Array of field names which filters should be shown by default in the table header
+  #       # items_per_page 100    # Override default_items_per_page
+  #       # sort_by :id           # Sort column (default is primary key)
+  #       # sort_reverse true     # Sort direction (default is true for primary key, last created first)
+  #     end
+  #     show do; end
+  #     edit do; end
+  #     export do; end
+  #     # also see the create, update, modal and nested sections, which override edit in specific cases (resp. when creating, updating, modifying from another model in a popup modal or modifying from another model nested form)
+  #     # you can override a cross-section field configuration in any section with the same syntax `configure :field_name do ... end`
+  #     # using `field` instead of `configure` will exclude all other fields and force the ordering
   # end
-  # config.model User do
-  #   # Found associations:
-  #     configure :repositories, :has_many_association   #   # Found columns:
-  #     configure :id, :integer
-  #     configure :email, :string
-  #     configure :password, :password         # Hidden
-  #     configure :password_confirmation, :password         # Hidden
-  #     configure :reset_password_token, :string         # Hidden
-  #     configure :reset_password_sent_at, :datetime
-  #     configure :remember_created_at, :datetime
-  #     configure :sign_in_count, :integer
-  #     configure :current_sign_in_at, :datetime
-  #     configure :last_sign_in_at, :datetime
-  #     configure :current_sign_in_ip, :string
-  #     configure :last_sign_in_ip, :string
-  #     configure :created_at, :datetime
-  #     configure :updated_at, :datetime
-  #     configure :github_uid, :integer
-  #     configure :nickname, :string
-  #     configure :name, :string
-  #     configure :github_token, :string   #   # Sections:
-  #   list do; end
-  #   export do; end
-  #   show do; end
-  #   edit do; end
-  #   create do; end
-  #   update do; end
-  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 RailsBrakemanCom::Application.routes.draw do
+
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 
   post '/' => 'repositories#sync'
