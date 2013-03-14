90499a2d0827d21c4750bde74f6bcf8ce4745924
Richard Huang <flyerhzm@gmail.com>
2013-03-14 16:18:38 +0800

    use puma capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,9 @@ require 'capistrano_colors'
 require 'bundler/capistrano'
 
 require 'rvm/capistrano'
-set :rvm_ruby_string, 'ruby-1.9.3-p194@rails-brakeman.com'
+set :rvm_ruby_string, 'ruby-2.0.0-p0@rails-brakeman.com'
+
+require 'puma/capistrano'
 
 set :application, "rails-brakeman.com"
 set :repository,  "git@github.com:flyerhzm/rails-brakeman.com.git"
@@ -26,6 +28,5 @@ namespace :deploy do
   task :restart, roles: :app, except: { no_release: true } do
     migrate
     cleanup
-    run "god restart rails-brakeman-server"
   end
 end
