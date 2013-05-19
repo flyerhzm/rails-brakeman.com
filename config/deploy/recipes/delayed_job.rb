80f8266023c7680b27d14d9edccaa03f05e115c7
Richard Huang <flyerhzm@gmail.com>
2013-05-19 10:35:55 +0800

    run delayed_job only on db server

--- config/deploy/recipes/delayed_job.rb
+++ config/deploy/recipes/delayed_job.rb
@@ -1,7 +1,7 @@
 after  "deploy:restart", "delayed_job:restart"
 
 namespace :delayed_job do
-  task :restart do
+  task :restart, :roles => :db do
     run "sudo monit restart delayed_job.rails-brakeman.com"
   end
 end
