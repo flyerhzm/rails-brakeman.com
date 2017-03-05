7c9bbd6d0c08b98e31ef5baf8da7a57842f69ff6
Richard Huang <flyerhzm@gmail.com>
2017-03-05 10:26:48 +0800

    fix column migration for postgresql

--- db/migrate/20120512132956_change_github_uid_to_integer.rb
+++ db/migrate/20120512132956_change_github_uid_to_integer.rb
@@ -1,6 +1,6 @@
 class ChangeGithubUidToInteger < ActiveRecord::Migration
   def up
-    change_column :users, :github_uid, :integer
+    change_column :users, :github_uid, 'integer USING CAST(github_uid AS integer)'
   end
 
   def down
--- db/schema.rb
+++ db/schema.rb
@@ -9,29 +9,32 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130204142716) do
+ActiveRecord::Schema.define(version: 20130204142716) do
 
-  create_table "builds", :force => true do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "builds", force: :cascade do |t|
     t.integer  "repository_id"
     t.string   "last_commit_id"
     t.string   "last_commit_message"
     t.integer  "position"
-    t.integer  "duration",            :default => 0
+    t.integer  "duration",            default: 0
     t.datetime "finished_at"
     t.string   "branch"
-    t.datetime "created_at",                         :null => false
-    t.datetime "updated_at",                         :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "aasm_state"
-    t.integer  "warnings_count",      :default => 0, :null => false
+    t.integer  "warnings_count",      default: 0, null: false
   end
 
-  add_index "builds", ["repository_id"], :name => "index_builds_on_repository_id"
+  add_index "builds", ["repository_id"], name: "index_builds_on_repository_id", using: :btree
 
-  create_table "delayed_jobs", :force => true do |t|
-    t.integer  "priority",   :default => 0
-    t.integer  "attempts",   :default => 0
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer  "priority",   default: 0
+    t.integer  "attempts",   default: 0
     t.text     "handler"
     t.text     "last_error"
     t.datetime "run_at"
@@ -39,26 +42,26 @@ ActiveRecord::Schema.define(:version => 20130204142716) do
     t.datetime "failed_at"
     t.string   "locked_by"
     t.string   "queue"
-    t.datetime "created_at",                :null => false
-    t.datetime "updated_at",                :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
-  create_table "rails_admin_histories", :force => true do |t|
+  create_table "rails_admin_histories", force: :cascade do |t|
     t.text     "message"
     t.string   "username"
     t.integer  "item"
     t.string   "table"
-    t.integer  "month",      :limit => 2
-    t.integer  "year",       :limit => 8
-    t.datetime "created_at",              :null => false
-    t.datetime "updated_at",              :null => false
+    t.integer  "month",      limit: 2
+    t.integer  "year",       limit: 8
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
+  add_index "rails_admin_histories", ["item", "table", "month", "year"], name: "index_rails_admin_histories", using: :btree
 
-  create_table "repositories", :force => true do |t|
+  create_table "repositories", force: :cascade do |t|
     t.integer  "github_id"
     t.string   "github_name"
     t.string   "name"
@@ -67,41 +70,41 @@ ActiveRecord::Schema.define(:version => 20130204142716) do
     t.boolean  "private"
     t.boolean  "fork"
     t.datetime "pushed_at"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.datetime "last_build_at"
     t.string   "html_url"
     t.string   "ssh_url"
-    t.integer  "builds_count",         :default => 0
+    t.integer  "builds_count",         default: 0
     t.integer  "user_id"
     t.string   "authentication_token"
-    t.boolean  "visible",              :default => true
-    t.boolean  "rails",                :default => true, :null => false
+    t.boolean  "visible",              default: true
+    t.boolean  "rails",                default: true, null: false
   end
 
-  add_index "repositories", ["user_id"], :name => "index_repositories_on_user_id"
+  add_index "repositories", ["user_id"], name: "index_repositories_on_user_id", using: :btree
 
-  create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "",    :null => false
-    t.string   "encrypted_password",     :default => "",    :null => false
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                :null => false
-    t.datetime "updated_at",                                :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "github_uid"
     t.string   "nickname"
     t.string   "name"
     t.string   "github_token"
-    t.boolean  "admin",                  :default => false, :null => false
+    t.boolean  "admin",                  default: false, null: false
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
