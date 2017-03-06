002e61385a49226977b357549ffca6adaed74aec
Richard Huang <flyerhzm@gmail.com>
2017-03-06 11:30:50 +0800

    move to https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ RailsBrakemanCom::Application.configure do
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # See everything in the log (default is :info)
   config.log_level = :debug
@@ -44,7 +44,7 @@ RailsBrakemanCom::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "http://assets.rails-brakeman.com"
+  config.action_controller.asset_host = "https://assets.rails-brakeman.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
