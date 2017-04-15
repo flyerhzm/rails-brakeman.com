b4c9ce61ae6df04529ec4492d62eba1d6e98857c
Richard Huang <flyerhzm@gmail.com>
2017-04-15 21:18:59 +0800

    contact us

--- config/initializers/contact_us.rb
+++ config/initializers/contact_us.rb
@@ -6,7 +6,7 @@ ContactUs.setup do |config|
   # Configure the e-mail address which email notifications should be sent from.  If emails must be sent from a verified email address you may set it here.
   # Example:
   # config.mailer_from = "contact@please-change-me.com"
-  config.mailer_from = nil
+  config.mailer_from = "postmaster@rails-brakeman.com"
 
   # Configure the e-mail address which should receive the contact form email notifications.
   config.mailer_to = "contact-us@rails-brakeman.com"
@@ -20,6 +20,15 @@ ContactUs.setup do |config|
   config.require_subject = true
 
   # Configure the form gem to use.
-  config.form_gem = "simple_form"
+  # Example:
+  # config.form_gem = 'formtastic'
+  config.form_gem = 'simple_form'
+
+  # Configure the redirect URL after a successful submission
+  config.success_redirect = '/'
+
+  # Configure the parent action mailer
+  # Example:
+  # config.parent_mailer = "ActionMailer::Base"
 
 end
