f0e4bbd6a032db853c980ebfd4284bca324a128b
Richard Huang <flyerhzm@gmail.com>
2017-04-16 21:24:55 +0800

    update notice_message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,15 @@
 module ApplicationHelper
+  ALERT_CLASS_NAMES = {
+    notice: :success,
+    alert: :danger,
+    error: :danger
+  }
   def notice_message
     flash_messages = []
 
     flash.each do |type, message|
-      type = :success if type == :notice
-      text = content_tag(:div, link_to("x", "#", class: "close", "data-dismiss" => "alert") + message, class: "alert alert-#{type}")
+      type = ALERT_CLASS_NAMES[type.to_sym] || type.to_sym
+      text = content_tag(:div, message, class: "alert alert-#{type}")
       flash_messages << text if message
     end
 
