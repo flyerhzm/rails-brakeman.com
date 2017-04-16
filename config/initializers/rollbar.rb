e21eb6c14a8fbc980505d3918e8750316aa03799
Richard Huang <flyerhzm@gmail.com>
2017-04-16 21:53:34 +0800

    prevent rollbar from reporting error in the development environment

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -5,7 +5,7 @@ Rollbar.configure do |config|
   config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
 
   # Here we'll disable in 'test':
-  if Rails.env.test?
+  if Rails.env.development? || Rails.env.test?
     config.enabled = false
   end
 
