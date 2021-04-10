d216b592cb09c7ba5cdafa832d2f3a3d9a90c739
Awesome Code <team@xinminlabs.com>
2021-04-10 06:14:33 +0000

    Auto corrected by following Lint Ruby Performance/RedundantMerge

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -27,7 +27,7 @@ Rollbar.configure do |config|
   # via the rollbar interface.
   # Valid levels: 'critical', 'error', 'warning', 'info', 'debug', 'ignore'
   # 'ignore' will cause the exception to not be reported at all.
-  config.exception_level_filters.merge!('ActionController::RoutingError' => 'ignore')
+  config.exception_level_filters['ActionController::RoutingError'] = 'ignore'
   #
   # You can also specify a callable, which will be called with the exception instance.
   # config.exception_level_filters.merge!('MyCriticalException' => lambda { |e| 'critical' })
